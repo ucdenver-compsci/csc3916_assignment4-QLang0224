9ffb06f3a04dafa06fff7e9a5e31badda10d77e2
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-04-13 09:15:23 -0600

    Update Reviews.js

--- Reviews.js
+++ Reviews.js
@@ -3,10 +3,13 @@ var Schema = mongoose.Schema;
 
 mongoose.connect(process.env.DB);
 
-// Movie schema
+// Review schema
 var ReviewSchema = new Schema({
-
+    movieId: { type: Schema.Types.ObjectId, ref: 'Movie', required: true }, // Reference to the Movie collection
+    username: { type: String, required: true },
+    review: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 }
 });
 
-// return the model
+// Return the model
 module.exports = mongoose.model('Review', ReviewSchema);
